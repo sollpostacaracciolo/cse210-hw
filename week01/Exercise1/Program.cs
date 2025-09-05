448a03691548b4a698ffd061c3ed36237f5ea9ca
sollpostacaracciolo <jazzcaracciolo@gmail.com>
2025-09-05 20:05:04 -0300

    1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine("your name is" + name + "," + lname + name);
+
     }
 }
+
